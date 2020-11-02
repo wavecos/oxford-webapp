b2c1a2ebed6d5e75d75091b21e0b5e633bf73fe6
onix <alfredo@xiobit.com>
2020-11-02 09:12:11 -0400

    definition retreived

--- app/controllers/oxford_controller.rb
+++ app/controllers/oxford_controller.rb
@@ -3,5 +3,35 @@ class OxfordController < ApplicationController
   end
 
   def search
+    search_term(params[:term])
+  end
+
+  private
+  def search_term(term)
+    url = "https://od-api.oxforddictionaries.com/api/v2/entries/en-us/#{term.downcase}"
+    response = Excon.get(
+      url,
+      headers: {
+        'app_id' => Rails.application.credentials.app_id,
+        'app_key' => Rails.application.credentials.app_key
+      }
+    )
+    if response.status != 200
+      begin
+        errorMsg = JSON.parse(response.body)['error'] # Check if the error is JSON obj
+      rescue JSON::ParserError => e  
+        errorMsg = response.body # Error msg is plain text
+      end 
+
+      flash[:alert] = errorMsg
+      return render action: :index
+    else
+      resp = JSON.parse(response.body)
+      @word = resp['word']
+      @definition = resp['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]   
+      @provider = resp['metadata']['provider']
+      @audio_url = resp['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][1]['audioFile']
+    end
+
   end
 end
