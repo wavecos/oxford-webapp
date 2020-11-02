ef2728bcc8524a3124af773bd523f880e9574df9
onix <alfredo@xiobit.com>
2020-11-02 07:43:08 -0400

    minor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'oxford#index'
-  get 'oxford/search'
+  get '/search' => 'oxford#search'
   
 end
